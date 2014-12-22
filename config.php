8e7024ac9951224e7fd6f062cb7505010dc77b47
Victor Argote <andrex289@hotmail.com>
2014-12-22 10:16:27 -0500

    Setting default owncloud config file

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc4492bf8830',
+  'passwordsalt' => '3ad65acc83bac98de680d0492d33c6',
+  'secret' => 'ed0b7bf613d51da3770dff2b305390d5299cc37b33305836efca022b73bfdcb7c653a63aa3c408c7d855d285d294cc79',
+  'trusted_domains' =>
+  array (
+    0 => '192.168.0.64',
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.0.64',
+  'dbtype' => 'pgsql',
+  'version' => '7.0.4.2',
+  'dbname' => 'owncloud_sm',
+  'dbhost' => '192.168.0.90',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => '8e173e6652bcba26ca1d9308549b2c',
+  'installed' => true,
+);
+
+
